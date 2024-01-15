42e6ed7e3ec0c0c553f24b4a727e451b7abc7aaa
Joffre Veloz <joffreandres11@gmail.com>
2024-01-15 01:51:10 -0500

    Update create-sensor.dto.ts

--- src/sensors/dto/create-sensor.dto.ts
+++ src/sensors/dto/create-sensor.dto.ts
@@ -1,15 +1,12 @@
 import { IsBoolean, IsOptional, IsString, MinLength } from "class-validator";
 
 export class CreateSensorDto {
-    @IsString()
     @MinLength(1)
     humidity: number;
     
-    @IsString()
     @MinLength(1)
     temperature: number;
     
     @IsBoolean()    
-    // @IsOptional()
     sensorStatus: boolean;
 }
